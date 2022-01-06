9c80919c37f670185a1b970055aabb0149b8fc5a
Raksha-Nanya <97081492+Raksha-Nanya@users.noreply.github.com>
2022-01-06 17:27:08 +0530

    Add files via upload

---
+++ Python/Activities/Activity4.py
@@ -0,0 +1,42 @@
+player1 = input("Please enter the player1 name ")
+player2 = input("Please enter the player2 name ")
+
+i=0
+while (i<10):
+    player1Answer = input(player1 +" Do you want to select Rock or Paper or Scissors: " )
+    player2Answer = input(player2 +" Do you want to select  Rock or Paper or Scissors: ")
+    
+    if player1Answer==player2Answer:
+        print("It's a tie")
+
+    elif player1Answer == 'Rock':
+        if player2Answer == 'Scissors':
+         print(player1 + " - Rock wins")
+        else:
+            print(player2 + " - Paper wins")
+
+    elif player1Answer == 'Scissors':
+        if player2Answer == 'Paper':
+            print(player1 + "Scissors wins")
+        else:
+            print(player2 + " - Rock wins")
+    elif player1Answer== "Paper":
+        if player2Answer == "Rock":
+            print(player1+ "- Paper wins")
+        else:
+            print(player2 + " - Scissors wins")
+    else:
+        print("one of the player have given invalid input")
+
+    repeat = input("Do you want to play another game? please enter Yes/No : ")
+    if (repeat=='Yes'):
+        pass
+    elif (repeat == 'No'):
+        break
+    else:
+        print("You have enter the wrong input. Terminating the Game")
+        break
+
+
+
+
